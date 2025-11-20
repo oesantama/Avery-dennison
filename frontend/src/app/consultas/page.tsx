6b1f7bc663fa03e12302497e48c16586df9b785a
oesantama <oscars.santamaria@gmail.com>
2025-11-20 08:01:52 -0500

    otro ajsute

--- frontend/src/app/consultas/page.tsx
+++ frontend/src/app/consultas/page.tsx
@@ -1,5 +1,16 @@
-import { useEffect } from 'react';
+"use client";
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function ConsultasRedirectPage() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/consultas/entregas');
+  }, [router]);
+  return null;
+}
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function ConsultasRedirectPage() {
   const router = useRouter();
